e557ebf59f910c8e07aa461a890497100ea48dd4
Sandesh9834 <78275234+Sandesh9834@users.noreply.github.com>
2023-06-19 23:23:55 +0530

    create - login and sinup page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {Route, Routes} from 'react-router-dom'
 import { HomePage } from './pages/home/HomePage';
 import { LandingPage } from './pages/landing/LandingPage';
+import { LoginPage } from './pages/login/LoginPage';
+import { SignUpPage } from './pages/signup/SignUpPage';
 import './App.css';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path = "/" element = {<LandingPage />} />
         <Route path = "/home" element = {<HomePage />} />
+        <Route path = "/login" element = {<LoginPage />} />
+        <Route path = "/signup" element = {<SignUpPage />} />
       </Routes>
     </div>
   );
