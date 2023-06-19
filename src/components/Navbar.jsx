4bdf88a2be463de937511a9115c62a22d3a8396e
Sandesh9834 <78275234+Sandesh9834@users.noreply.github.com>
2023-06-20 01:05:48 +0530

    feat - navbar, sidebar components created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export function Navbar(){
+  return(
+    <>
+      <nav className="white-bg">
+        <div className="p-s pl-xxl txt-m ml-m">
+          <a href="../../index.html">
+            <span className="primary-color">My</span>Website
+          </a>
+        </div>
+      </nav>
+    </>
+  )
+}
