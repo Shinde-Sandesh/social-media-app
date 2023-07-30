b48fc0e5ded192c5907d35788890e806fed8c7a8
Sandesh Suhas Shinde <78275234+Sandesh9834@users.noreply.github.com>
2023-07-30 05:54:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path = "/home" element = {<LandingPage />} />
-        <Route path = "/home" element = {<HomePage />} />
+        <Route path = "/" element = {<HomePage />} />
         {/* <Route path = "/home" element = {<RequiresAuth><HomePage /></RequiresAuth>} /> */}
         <Route path = "/login" element = {<LoginPage />} />
         <Route path = "/signup" element = {<SignUpPage />} />
